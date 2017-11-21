e60325399554608a465bae1c033413c447b876f9
Kevin Boucher <kevin@kevinboucher.com>
2017-11-21 14:16:48 -0600

    Adds category tree filter

--- src/app/category/components/tree-view.component.ts
+++ src/app/category/components/tree-view.component.ts
@@ -4,6 +4,12 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Category } from '../models/category';
 import { OnInit } from '@angular/core';
 
+const treeSelector = '#category-tree';
+
+/**
+    Recursively searches multidimensional array of objects
+    to find one with a particular ID value.
+ */
 function findById(collection, id) {
     if (collection.length) {
         for (var i = 0; i < collection.length; i++) {
@@ -29,21 +35,31 @@ export class TreeViewComponent implements OnInit {
     constructor() {}
 
     ngOnInit() {
-        console.log('Categories tree-view component initialized with ' + this.categories.length + ' categories.');
+        /*
+            Configures jstree to hide nodes that do not match the filter
+            search string. May be source of performance issues on large
+            trees or older browsers: https://www.jstree.com/api/#/?f=$.jstree.defaults.search.show_only_matches
+        */
+        $.jstree.defaults.search.show_only_matches = true;
 
-        $('#category_tree').on('select_node.jstree', function(e, data) {
+        $(treeSelector).on('select_node.jstree', function(e, data) {
             let category = findById(this.categories, data.node.id);
 
             this.select(category);
         }.bind(this)).jstree({
             'core': {
                 'data': this.categories
-            }
+            },
+            'plugins': [ 'search' ]
         });
-    }
 
-    ngOnChanges() {
-        //alert('Tree view onChanges fired!');
+        /*
+            Initializes tree view filter, can also be amended to process
+            on form submit instead of keyup event.
+        */
+        $("#category-tree-filter-input").keyup(function(e) {
+            $(treeSelector).jstree(true).search($("#category-tree-filter-input").val());
+        });
     }
 
     select(category: Category) {
